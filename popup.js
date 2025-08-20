f9d094ef2b43ae4641caa172ee2f98cb207954e4
rajnishsharma07 <144888340+rajnishsharma07@users.noreply.github.com>
2025-08-20 23:10:34 +0530

    changes

---
+++ popup.js
@@ -0,0 +1,6 @@
+function openPopup() {
+    document.getElementById("popupOverlay").style.display = "flex";
+  }
+  function closePopup() {
+    document.getElementById("popupOverlay").style.display = "none";
+  }
